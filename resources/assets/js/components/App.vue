2e7c5772de9e4a61e6507649b04265830500386d
史明哲 <shimingzhe@shimingzhedeMacBook-Pro.local>
2018-12-17 01:15:27 +0800

    update

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -87,20 +87,18 @@
 
             this.$http.get('/getSignature/'+this.$route.params.uniqId).then(response => {
                 let data = response.body;
-                wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
-                    wx.config({
-                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                        appId: data.appId, // 必填，公众号的唯一标识
-                        timestamp: data.timestamp, // 必填，生成签名的时间戳
-                        nonceStr: data.nonceStr, // 必填，生成签名的随机串
-                        signature: data.signature,// 必填，签名
-                        jsApiList: [
-                            'updateAppMessageShareData',
-                            'updateTimelineShareData',
-                            'onMenuShareWeibo',
-                            'onMenuShareQZone',
-                        ] // 必填，需要使用的JS接口列表
-                    });
+                wx.config({
+                    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    appId: data.appId, // 必填，公众号的唯一标识
+                    timestamp: data.timestamp, // 必填，生成签名的时间戳
+                    nonceStr: data.nonceStr, // 必填，生成签名的随机串
+                    signature: data.signature,// 必填，签名
+                    jsApiList: [
+                        'updateAppMessageShareData',
+                        'updateTimelineShareData',
+                        'onMenuShareWeibo',
+                        'onMenuShareQZone',
+                    ] // 必填，需要使用的JS接口列表
                 });
 
                 wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
