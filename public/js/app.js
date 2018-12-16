489ffbcd416f07a82d0403c1c65f5d197ee3d578
史明哲 <shimingzhe@shimingzhedeMacBook-Pro.local>
2018-12-17 00:36:16 +0800

    分享功能

--- public/js/app.js
+++ public/js/app.js
@@ -53185,7 +53185,35 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
                                 __WEBPACK_IMPORTED_MODULE_1_jquery___default()("#lrc_content").html(html);
                             });
 
-                        case 2:
+                            this.$http.get('/getSignature').then(function (response) {
+                                console.log(response.body);
+                                var data = JSON.parse(response.body);
+                                wx.ready(function () {
+                                    //需在用户可能点击分享按钮前就先调用
+                                    wx.config({
+                                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                                        appId: data.appId, // 必填，公众号的唯一标识
+                                        timestamp: data.timestamp, // 必填，生成签名的时间戳
+                                        nonceStr: data.nonceStr, // 必填，生成签名的随机串
+                                        signature: data.signature, // 必填，签名
+                                        jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData', 'onMenuShareWeibo', 'onMenuShareQZone'] // 必填，需要使用的JS接口列表
+                                    });
+                                });
+
+                                wx.ready(function () {
+                                    //需在用户可能点击分享按钮前就先调用
+                                    wx.updateAppMessageShareData({
+                                        title: '分享标题', // 分享标题
+                                        desc: '分享描述', // 分享描述
+                                        link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                                        imgUrl: '', // 分享图标
+                                        success: function success() {
+                                            // 设置成功
+                                        } });
+                                });
+                            });
+
+                        case 3:
                         case "end":
                             return _context.stop();
                     }
@@ -53215,22 +53243,7 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
         bottomDisplayClose: function bottomDisplayClose() {
             this.is_close = false;
         },
-        share: function share() {
-            this.$http.get('/getSignature').then(function (response) {
-                var data = JSON.parse(response.body);
-                wx.ready(function () {
-                    //需在用户可能点击分享按钮前就先调用
-                    wx.config({
-                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                        appId: data.appId, // 必填，公众号的唯一标识
-                        timestamp: data.timestamp, // 必填，生成签名的时间戳
-                        nonceStr: data.nonceStr, // 必填，生成签名的随机串
-                        signature: data.signature, // 必填，签名
-                        jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData', 'onMenuShareWeibo', 'onMenuShareQZone'] // 必填，需要使用的JS接口列表
-                    });
-                });
-            });
-        },
+        share: function share() {},
         getData: function getData(offset) {
             var _this2 = this;
 
--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -84,6 +84,37 @@
                 this.lrc_content = lrc_content;
                 $("#lrc_content").html(html);
             });
+
+            this.$http.get('/getSignature').then(response => {
+                console.log(response.body);
+                let data = JSON.parse(response.body);
+                wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+                    wx.config({
+                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        appId: data.appId, // 必填，公众号的唯一标识
+                        timestamp: data.timestamp, // 必填，生成签名的时间戳
+                        nonceStr: data.nonceStr, // 必填，生成签名的随机串
+                        signature: data.signature,// 必填，签名
+                        jsApiList: [
+                            'updateAppMessageShareData',
+                            'updateTimelineShareData',
+                            'onMenuShareWeibo',
+                            'onMenuShareQZone',
+                        ] // 必填，需要使用的JS接口列表
+                    });
+                });
+
+                wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+                    wx.updateAppMessageShareData({
+                        title: '分享标题', // 分享标题
+                        desc: '分享描述', // 分享描述
+                        link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                        imgUrl: '', // 分享图标
+                        success: function () {
+                            // 设置成功
+                        }});
+                    });
+            });
         },
         methods: {
             audioPlay() {
@@ -101,24 +132,7 @@
                 this.is_close = false;
             },
             share() {
-                this.$http.get('/getSignature').then(response => {
-                    let data = JSON.parse(response.body);
-                    wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
-                        wx.config({
-                            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                            appId: data.appId, // 必填，公众号的唯一标识
-                            timestamp: data.timestamp, // 必填，生成签名的时间戳
-                            nonceStr: data.nonceStr, // 必填，生成签名的随机串
-                            signature: data.signature,// 必填，签名
-                            jsApiList: [
-                                'updateAppMessageShareData',
-                                'updateTimelineShareData',
-                                'onMenuShareWeibo',
-                                'onMenuShareQZone',
-                            ] // 必填，需要使用的JS接口列表
-                        });
-                    });
-                });
+
             },
             getData(offset) {
                 this.$http.get('/have_a_look/'+offset).then(response => {
