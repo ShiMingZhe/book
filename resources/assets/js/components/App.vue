31f1e965f8b1a8c96fbec68eefdd71402547a87a
史明哲 <shimingzhe@shimingzhedeMacBook-Pro.local>
2018-12-17 21:20:30 +0800

    update

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -101,18 +101,19 @@
                         'showOptionMenu',
                         "hideMenuItems",
                         "showMenuItems",
-                        "onMenuShareTimeline",
-                        'onMenuShareAppMessage',
                     ] // 必填，需要使用的JS接口列表
                 });
 
                 wx.ready(function () {      //需在用户可能点击分享按钮前就先调用
-                    wx.updateTimelineShareData({
-                        title: '', // 分享标题
+                    wx.onMenuShareAppMessage({
+                        title: '分享标题', // 分享标题
+                        desc: '分享描述', // 分享描述
                         link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                         imgUrl: '', // 分享图标
+                        type: '', // 分享类型,music、video或link，不填默认为link
+                        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
                         success: function () {
-                            // 设置成功
+                            // 用户点击了分享后执行的回调函数
                         }
                     });
                 });
