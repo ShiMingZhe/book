b73892e9f6005ccba2bdbcbd84e0dfcb37c6aa54
史明哲 <shimingzhe@shimingzhedeMacBook-Pro.local>
2019-01-19 16:35:24 +0800

    增加数组分页

--- resources/views/admin/data/music.blade.php
+++ resources/views/admin/data/music.blade.php
@@ -6,14 +6,16 @@
         <h3>诗词音频列表</h3>
     </div>
     <div class="box-body">
-        <table class="table table-bordered table-striped">
+        <table class="table table-hover">
             @foreach($data as $key => $value)
                 <tr><td>{{$key}}</td><td>http://music.mukeen.com/{{$value}}</td></tr>
             @endforeach
         </table>
     </div>
-    <div class="box-footer">
-
+    <div class="box-footer clearfix">
+        <ul class="pagination pagination-sm no-margin pull-right">
+            {{$dataInfo->links()}}
+        </ul>
     </div>
 </div>
 
