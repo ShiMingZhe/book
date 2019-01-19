1e45450a0b9a550eb6e413b45a7dd45441c2b03d
史明哲 <shimingzhe@shimingzhedeMacBook-Pro.local>
2019-01-19 16:32:29 +0800

    增加数组分页

--- app/Http/Controllers/DatasController.php
+++ app/Http/Controllers/DatasController.php
@@ -9,6 +9,9 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+
 class DatasController
 {
     public function index() {
@@ -22,7 +25,22 @@ class DatasController
         unset($data[0]);
         unset($data[1]);
         $data = array_values($data);
+        //获取当前的分页数 初始是1
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        //实例化collect方法
+        $collection = new Collection($data);
+        //定义每页显示多少数据
+        $perPage = 10;
+        //获取当前需要显示的数据列表
+        $currentPageSearchResults = $collection->slice(($currentPage-1)*$perPage, $perPage)->all();
+        //创建一个新的分页方法
+        $paginateSearchResults_a = new LengthAwarePaginator($currentPageSearchResults, count($collection), $perPage);
+        //存储路径防止下一页路径丢失
+        $dataInfo = $paginateSearchResults_a->setPath('/music/list');
 
-        return view('admin.data.music', ['data' => $data]);
+        return view('admin.data.music', [
+            'data' => $data,
+            'dataInfo' => $dataInfo,
+        ]);
     }
 }
--- resources/views/admin/data/music.blade.php
+++ resources/views/admin/data/music.blade.php
@@ -1,11 +1,22 @@
 {{--@include('admin.header')--}}
 @extends('admin.template.layouts')
 @section('content')
-<table class="table table-bordered table-striped">
-    @foreach($data as $key => $value)
-        <tr><td>{{$key}}</td><td>http://music.mukeen.com/{{$value}}</td></tr>
-    @endforeach
-</table>
+<div class="box">
+    <div class="box-header">
+        <h3>诗词音频列表</h3>
+    </div>
+    <div class="box-body">
+        <table class="table table-bordered table-striped">
+            @foreach($data as $key => $value)
+                <tr><td>{{$key}}</td><td>http://music.mukeen.com/{{$value}}</td></tr>
+            @endforeach
+        </table>
+    </div>
+    <div class="box-footer">
+
+    </div>
+</div>
+
 @endsection
 {{--
 @include('admin.footer')--}}
