be177f96a9da5166cc17f1afbca64b0f798ef21a
史明哲 <shimingzhe@shimingzhedeMacBook-Pro.local>
2019-02-07 11:41:26 +0800

    更新

--- database/migrations/2019_02_03_005913_alter_outer_brands_table.php
+++ database/migrations/2019_02_03_005913_alter_outer_brands_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterOuterBrandsTable1 extends Migration
+class AlterOuterBrandsTable extends Migration
 {
     /**
      * Run the migrations.
